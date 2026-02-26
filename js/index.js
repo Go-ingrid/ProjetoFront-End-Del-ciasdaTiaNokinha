a3829f057d1b05c681d889758d2aa7596e303ec4
Ingrid G. Torres <161876114+Go-ingrid@users.noreply.github.com>
2026-02-26 09:23:42 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,22 @@
-// Dados dos slides: imagem e texto descritivo (o <p> vem logo após o <img>)
+// Dados dos slides: imagem e texto descritivo
 const dadosSlides = [
   {
-    src: "bolo.jpg",
-
+    src: "imagem/bolo.jpg",   
     texto:
       "Curso de Bolos Caseiros e Festa (Online).<br> Aprenda a preparar massas fofinhas, recheios equilibrados e montagens perfeitas para bolos caseiros e de festa. Você vai entender técnicas simples, combinações que vendem e como transformar cada receita em uma fonte de renda. <br><br> Ideal para quem quer começar do zero ou melhorar seus resultados na cozinha",
   },
   {
-    src: "salgados.jpg",
-
+    src: "imagem/salgados.jpg", 
     texto:
       "Curso de Salgadinhos Fritos e Assados (Online).<br>Descubra o passo a passo para preparar salgadinhos saborosos e padronizados, como coxinha, bolinha de queijo, kibe e muito mais. Você vai aprender desde a massa até o ponto ideal de fritura ou forno, garantindo qualidade e lucro em cada produção.<br><br> Perfeito para quem deseja vender para festas, encomendas e eventos.",
   },
   {
-    src: "doce.jpg",
-
+    src: "imagem/doce.jpg",  
     texto:
       "Curso de Doces Gourmet (Online ou Presencial Premium).<br> Aprenda a produzir doces gourmet com acabamento profissional e alto valor agregado. Brigadeiros especiais, combinações sofisticadas e técnicas de apresentação que encantam e vendem. Na versão presencial premium, você também aprende técnicas de finalização e decoração com acompanhamento individual. <br><br>Ideal para quem quer elevar o nível e aumentar o faturamento.",
   },
   {
-    src: "confeitar.jpg",
-
+    src: "imagem/confeitar.jpg",  
     texto:
       "Curso de Confeitaria Presencial (Turma Exclusiva – até 2 alunas),<br> Uma experiência prática e personalizada para quem quer dominar técnicas de confeitaria com segurança. Aprenda na prática montagem, nivelamento, recheio e finalização de bolos com orientação direta. <br><br> Vagas limitadas para até 2 alunas por turma, garantindo atenção individual e aprendizado completo.",
   },
@@ -63,7 +59,6 @@ perguntas.forEach((pergunta) => {
     item.classList.toggle("ativo");
   });
 });
-
 
 // BOTÃO VOLTAR AO TOPO
 const voltarTopo = document.getElementById("voltarTopo");
